aeed20cd2f619dd4fddc45d6b79f977e15a6e8b2
Jishnu Prasad Samal <dev@jishnupsamal.me>
2025-10-27 19:36:07 +0530

    feat: enhance substring deletion functionality in string manipulation program

--- Lab - 7/04_delete_a_word/delete_a_word.c
+++ Lab - 7/04_delete_a_word/delete_a_word.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-int findSubstring (char str[], char substr[]);
-
 int main() {
-    char str[100], substr[100];
-    int index = 0;
+    char str[100], substr[100], newStr[100][100];
+    int i = 0, j = 0, n = 0;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+    if (str[strlen(str) - 1] == '\n') str[strlen(str) - 1] = '\0';
 
     printf("Enter a substring to be deleted: ");
     fgets(substr, sizeof(substr), stdin);
+    if (substr[strlen(substr) - 1] == '\n') substr[strlen(substr) - 1] = '\0';
 
-    int n = strlen(str);
-    int m = strlen(substr);
+    while (1) {
+        if (str[i] != ' ' && str[i] != '\0') {
+            newStr[n][j++] = str[i];
+        } else {
+            // i++
+            if (str[i] == ' ' || str[i] == '\0') {
+                newStr[n][j++] = '\0';
+            }
+            j = 0;
+            n++;
+            if (str[i] == '\0') {
+                break;
+            }
+        }
+        i++;
+    }
+    
+    char temp[100] = "";
+
+    for (int j = 0; j < n; j++) {
+        if(strcmp(newStr[j], substr) == 0) {
+            n--;
+            for (int k = j; k < n; k++) {
+                strcpy(newStr[k], newStr[k+1]);
+            }
+        }
+    }
 
 
+    for (int l = 0; l < n; l++) {
+        strcat(temp, newStr[l]);
+        strcat(temp, " ");
+    }
+
+    strcpy(str, temp);
     puts(str);
 }
